5a89d2f64e866e02dc11f0e945bc1e8be6b1551b
Johnny Choo <jchoo156@gmail.com>
2018-06-14 18:22:17 -0700

    tweek

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './src/js/index.js'
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, '/dist'),
     filename: 'bundle.js'
   },
   module: {
